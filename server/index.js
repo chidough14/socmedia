49ea2f0bd62f8df83cbfe0735afb47bd0f64b03f
Chidozie <chidough14@gmail.com>
2022-11-25 06:42:17 +0100

    Authorization, posts,users routes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
 import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js"
+import { createPost } from "./controllers/posts.js"
 
 
 // Configurations
@@ -41,9 +44,12 @@ const upload = multer({ storage })
 
 //Routes with files
 app.post("/auth/register", upload.single("picture"), register)
+app.post("/posts", upload.single("picture"), createPost)
 
 //Routes
 app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
 
 //Mongoose setup
